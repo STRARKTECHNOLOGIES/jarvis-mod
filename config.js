4a3644535bd735037e5fb4c05d25a00c7cb5b76a
STRARKTECHNOLOGIES <70024979+STRARKTECHNOLOGIES@users.noreply.github.com>
2021-09-07 09:23:52 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.TOKEN = 'ODE2MTQxMDQxMjc0ODQ3MjQz.YD2okw.PN0uFAfTQW1ccn2PfGzzDyzgOxs';
+
+exports.PREFIX = '$';
+
+exports.news_API = '9cc063fcca2c4cf69e90890aadf70bd4';
+
+exports.giphy_API = 'ndyxUZDDFa0TSyVCVKTF7oIYx6aRLZMe';
+
+exports.AME_API = 'bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
